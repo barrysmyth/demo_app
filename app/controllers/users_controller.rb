3dc5638b12a7c5bc285ccec7f67342b90969b047
Barry Smyth <barry.smyth@gmail.com>
2012-06-27 12:09:34 +0100

    Simplified the user Users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,6 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
   end
 
   # GET /users/1
